a1465fcf940b135bb5d81814b31b37f07ca37643
Leonardo Vaz <leonardovazgremista@hotmail.com>
2022-11-07 20:42:33 -0300

    Project created

---
+++ src/main/java/com/leonardo/courseSpringUdemy/CourseSpringUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.courseSpringUdemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardo/courseSpringUdemy/CourseSpringUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.courseSpringUdemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
